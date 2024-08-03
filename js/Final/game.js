c690fb887bc528247664a262a231767de932c473
Zhnmsn <Zhnmsn@yandex.ru>
2024-08-03 18:40:36 +0300

    edit final 4 module

--- js/Final/game.js
+++ js/Final/game.js
@@ -35,7 +35,7 @@
     return function start() {
       const RockScissorPaper = () => {
         const userWord = prompt('Камень, ножницы, бумага ?');
-        const computerWord = getRandomIntInclusive(1, 10);
+
         if (userWord === null) {
           const quastion = confirm('Вы точно хотите выйти из игры?');
           if (quastion) {
@@ -45,48 +45,57 @@
           }
         }
 
-        if (userWord[0] === 'к' && computerWord === 'бумага' ||
+        if (userWord !== 'Камень' && userWord !== 'камень' && userWord !== 'бумага' &&
+          userWord !== 'Бумага' && userWord !== 'Ножницы' && userWord !== 'ножницы') {
+          alert('Вы ввели некорректно');
+          return RockScissorPaper();
+        }
+
+        const computerWord = getRandomIntInclusive(1, 10);
+
+
+        if (userWord === 'Камень' && computerWord === 'бумага' ||
                   userWord === 'камень' && computerWord === 'бумага') {
           alert('Вы проиграли, первый ходит компьютер!');
           return firstTurnComputer();
         }
 
-        if (userWord[0] === 'к' && computerWord === 'ножницы') {
+        if (userWord === 'камень' && computerWord === 'ножницы' ||
+                  userWord === 'Камень' && computerWord === 'ножницы') {
           alert('Вы выиграли, первый ход за вами!');
           return firstTurnUser();
         }
 
 
-        if (userWord[0] === 'н' && computerWord === 'камень') {
+        if (userWord === 'ножницы' && computerWord === 'камень' ||
+                  userWord === 'Ножницы' && computerWord === 'камень') {
           alert('Вы проиграли, первый ходит компьютер!');
           return firstTurnComputer();
         }
 
-        if (userWord[0] === 'н' && computerWord === 'бумага') {
+        if (userWord === 'ножницы' && computerWord === 'бумага' ||
+                  userWord === 'Ножницы' && computerWord === 'бумага') {
           alert('Вы выиграли, первый ход за вами!');
           return firstTurnUser();
         }
 
 
-        if (userWord[0] === 'б' && computerWord === 'ножницы') {
+        if (userWord === 'бумага' && computerWord === 'ножницы' ||
+                  userWord === 'Бумага' && computerWord === 'ножницы') {
           alert('Вы проиграли, первый ходит компьютер!');
           return firstTurnComputer();
         }
 
-        if (userWord[0] === 'б' && computerWord === 'камень') {
+        if (userWord === 'бумага' && computerWord === 'камень' ||
+                  userWord === 'Бумага' && computerWord === 'камень') {
           alert('Вы выиграли, первый ход за вами!');
           return firstTurnUser();
         }
 
-        if (userWord[0] === computerWord[0]) {
+        if (userWord[0] === computerWord[0] || userWord[0] === 'Б' && computerWord[0] === 'б' ||
+          userWord[0] === 'К' && computerWord[0] === 'к' || userWord[0] === 'Н' && computerWord[0] === 'н'
+        ) {
           alert('Ничья, сделайте ещё ход!');
-          return RockScissorPaper();
-        }
-
-
-        if (userWord.length > 7 || userWord[0] !== 'к' &&
-                  userWord[0] !== 'б' && userWord[0] !== 'н') {
-          alert('Вы ввели некорректно');
           return RockScissorPaper();
         }
       };
@@ -95,6 +104,16 @@
         alert('Сделайте первый ход!');
 
         const userNumber = +prompt(`Введите число от 1 до ${result.player}`);
+
+        if (userNumber === 0 || userNumber === null) {
+          const exit = confirm('Желаете покинуть игру?');
+          if (exit) {
+            return alert('До скорых встреч!');
+          } else {
+            alert('Продолжим игру!');
+          }
+        }
+
         if (userNumber < 0 || userNumber > result.player) {
           alert(`Введите число от 1 до ${result.player}`);
           return firstTurnUser();
@@ -105,27 +124,60 @@
         if (computerNumber % 2 === 0 && userNumber % 2 === 0) {
           result.player -= userNumber;
           result.computer += userNumber;
-          alert(`Компьютер выиграл! Число компьютера : ${computerNumber}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+
+          alert(`Компьютер выиграл! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
         if (computerNumber % 2 !== 0 && userNumber % 2 !== 0) {
           result.player -= userNumber;
           result.computer += userNumber;
-          alert('Компьютер выиграл!');
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Компьютер выиграл! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
         if (computerNumber % 2 === 0 && userNumber % 2 !== 0) {
           result.player += userNumber;
           result.computer -= userNumber;
-          alert('Вы выиграли!');
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Вы выиграли! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
         if (computerNumber % 2 !== 0 && userNumber % 2 === 0) {
           result.player += userNumber;
           result.computer -= userNumber;
-          alert('Вы выиграли!');
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Вы выиграли! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
         if (result.player < 1) {
           const goAgain = confirm('Победа Копмьютера, хотите поиграть еще?');
@@ -149,49 +201,69 @@
           }
         }
 
-        if (userNumber === 0 || userNumber === null) {
-          confirm('Желаете покинуть игру?');
-        }
-        if (userNumber === 0 || userNumber === null) {
-          return alert('До скорых встреч!');
-        }
-
 
         alert('А теперь ход сделает компьютер!');
 
         const compChois = getComputerChois(1, result.computer);
-        const userChois = +prompt('Введите 2 если думаете что четное, 1 если нет!');
-
-        if (userChois !== 1 && userChois !== 2) {
-          alert('Введите 1 или 2');
-        }
-
-        if (compChois % 2 === 0 && userChois === 2) {
+        const userChois = confirm('Число четное?');
+        if (userChois && compChois % 2 === 0) {
           result.player += compChois;
           result.computer -= compChois;
-          alert(`Вы выиграли! Число компьютера : ${compChois}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Вы выиграли! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
-        if (compChois % 2 === 0 && userChois === 1) {
+        if (userChois && compChois % 2 !== 0) {
           result.player -= compChois;
           result.computer += compChois;
-          alert(`Компьютер выиграл! Число компьютера : ${compChois}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Компьютер выиграл! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
-        if (compChois % 2 !== 0 && userChois === 2) {
+        if (!userChois && compChois % 2 === 0) {
           result.player -= compChois;
           result.computer += compChois;
-          alert(`Компьютер выиграл! Число компьютера : ${compChois}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Компьютер выиграл! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
-        if (compChois % 2 !== 0 && userChois === 1) {
+        if (!userChois && compChois % 2 !== 0) {
           result.player += compChois;
           result.computer -= compChois;
-          alert(`Вы выиграли! Число компьютера : ${compChois}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Вы выиграли! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
         if (result.player < 1) {
@@ -229,39 +301,70 @@
         alert('Первый ход сделает компьютер!');
 
         const compChois = getComputerChois(1, result.computer);
-        const userChois = +prompt('Введите 2 если четное, 1 если нет!');
 
-        if (userChois !== 1 && userChois !== 2) {
-          alert('Введите 1 или 2');
-        }
 
-        if (compChois % 2 === 0 && userChois === 2) {
+        const userChois = confirm('Число четное?');
+        if (userChois && compChois % 2 === 0) {
           result.player += compChois;
           result.computer -= compChois;
-          alert(`Вы выиграли! Число компьютера : ${compChois}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+
+          alert(`Вы выиграли! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
-        if (compChois % 2 === 0 && userChois === 1) {
+        if (userChois && compChois % 2 !== 0) {
           result.player -= compChois;
           result.computer += compChois;
-          alert(`Компьютер выиграл! Число компьютера : ${compChois}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Компьютер выиграл! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
-        if (compChois % 2 !== 0 && userChois === 2) {
+        if (!userChois && compChois % 2 === 0) {
           result.player -= compChois;
           result.computer += compChois;
-          alert(`Компьютер выиграл! Число компьютера : ${compChois}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Компьютер выиграл! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
-        if (compChois % 2 !== 0 && userChois === 1) {
+        if (!userChois && compChois % 2 !== 0) {
           result.player += compChois;
           result.computer -= compChois;
-          alert(`Вы выиграли! Число компьютера : ${compChois}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Вы выиграли! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
+
 
         if (result.player < 1) {
           const goAgain = confirm('Компьютер выиграл, хотите поиграть ещё?');
@@ -305,27 +408,59 @@
         if (computerNumber % 2 === 0 && userNumber % 2 === 0) {
           result.player -= userNumber;
           result.computer += userNumber;
-          alert(`Компьютер выиграл! Число компьютера : ${computerNumber}`);
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Компьютер выиграл! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
 
         if (computerNumber % 2 !== 0 && userNumber % 2 !== 0) {
           result.player -= userNumber;
           result.computer += userNumber;
-          alert('Компьютер выиграл!');
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Компьютер выиграл! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
         if (computerNumber % 2 === 0 && userNumber % 2 !== 0) {
           result.player += userNumber;
           result.computer -= userNumber;
-          alert('Вы выиграли!');
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Вы выиграли! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
         if (computerNumber % 2 !== 0 && userNumber % 2 === 0) {
           result.player += userNumber;
           result.computer -= userNumber;
-          alert('Вы выиграли!');
-          alert(`Игрок : ${result.player}, Компьютер : ${result.computer}`);
+          if (result.player < 0) {
+            result.player = 0;
+          } if (result.computer < 0) {
+            result.computer = 0;
+          } if (result.player > 10) {
+            result.player = 10;
+          } if (result.computer > 10) {
+            result.computer = 10;
+          }
+          alert(`Вы выиграли! Игрок : ${result.player}, Компьютер : ${result.computer}`);
         }
         if (result.player < 1) {
           const goAgain = confirm('Победа Копмьютера, хотите поиграть еще?');
